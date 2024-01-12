f14d95df6a1ad2bc5824fa705d6fff799f78387b
gistbit <muh.akram1422@gmail.com>
2024-01-12 23:07:26 +0800

    Menambahkan Class AddRoute

---
+++ app/core/Router/AddRoute.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Core\Router;
+
+final class Route {
+    
+    private $method;
+
+    private $pattern;
+
+    private $controller; 
+
+    private $action;
+
+    private $middleware;
+
+    public function __construct(String $method, String $pattern, $callback, $options = []) {
+        $this->method = $method;
+        $this->pattern = $pattern;
+        $this->parseCallback($callback);
+        $this->parseOption($options);
+    }
+
+    private function parseCallback($callback)
+    {
+        if (is_string($callback)) {
+            list($this->controller, $this->action) = $this->parseControllerAction($callback);
+        } elseif (is_callable($callback)) {
+            $this->controller = null;
+            $this->action = $callback;
+        } else {
+            throw new \InvalidArgumentException(print('Invalid callback provided'));
+        }
+    }
+
+    private function parseControllerAction($callback)
+    {
+        $segments = explode('@', $callback);
+        if (count($segments) !== 2) throw new \InvalidArgumentException(print('Invalid controller action format'));
+
+        $segments['0'] = "\App\Controllers\\".$segments['0'];
+
+        return $segments;
+    }
+
+    private function parseOption($options = []){
+
+        if (empty($option)) {
+            $this->middleware = null;
+        } else if (count($option) == 1) {
+            $this->middleware = current($option);
+            $this->middleware = new $this->middleware;
+        } else if (count($option) == 2) {
+            [$this->middleware, $role] = $option;
+            if(class_exists($this->middleware)){
+                $this->middleware = (new $this->middleware)->$role();
+            }else{
+                throw new \InvalidArgumentException(print('Invalid Class Middleware format'));
+            }
+        }
+    }
+
+    public function add()
+    {
+        return new Route($this);
+    }
+}

e944e68a3da0be72578bceb1db0caf3b7af25346
gistbit <muh.akram1422@gmail.com>
2024-01-12 21:55:18 +0800

    tambah colom role di table users

--- database.sql
+++ database.sql
@@ -3,7 +3,8 @@ CREATE DATABASE php_mvc;
 CREATE TABLE users(
     id VARCHAR(255) PRIMARY KEY ,
     name VARCHAR(255) NOT NULL ,
-    password VARCHAR(255) NOT NULL
+    password VARCHAR(255) NOT NULL,
+    role VARCHAR(255) NULL
 ) ENGINE InnoDB;
 
 CREATE TABLE sessions(

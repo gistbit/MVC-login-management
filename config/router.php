f2299b00432c1886ffba8f18cb932501f568f8b0
gistbit <muh.akram1422@gmail.com>
2024-02-02 18:36:53 +0800

    rename class

--- config/router.php
+++ config/router.php
@@ -1,26 +1,26 @@
 <?php
 
 use App\Core\MVC\View;
-use App\Middlewares\{MustLoginMiddleware, MustNotLoginMiddleware, MustLoginAdmin};
+use App\Middlewares\{OnlyMemberMiddleware, OnlyGuestMiddleware, MustLoginAdmin};
 
 use function App\helper\response;
 use App\Core\Http\Response;
 
 $router->get('/', 'HomeController@index');
 
-$router->get("/user/register", "UserController@register", [MustNotLoginMiddleware::class]);
-$router->post("/user/register", "UserController@postRegister", [MustNotLoginMiddleware::class]);
+$router->get("/user/register", "UserController@register", [OnlyGuestMiddleware::class]);
+$router->post("/user/register", "UserController@postRegister", [OnlyGuestMiddleware::class]);
 
-$router->get("/user/login", "UserController@login", [MustNotLoginMiddleware::class]);
-$router->post("/user/login", "UserController@postLogin", [MustNotLoginMiddleware::class]);
+$router->get("/user/login", "UserController@login", [OnlyGuestMiddleware::class]);
+$router->post("/user/login", "UserController@postLogin", [OnlyGuestMiddleware::class]);
 
-$router->get("/user/logout", "UserController@logout", [MustLoginMiddleware::class]);
+$router->get("/user/logout", "UserController@logout", [OnlyMemberMiddleware::class]);
 
 $router->get("/user/profile", "UserController@updateProfile", [MustLoginAdmin::class]);
-$router->post("/user/profile", "UserController@postUpdateProfile", [MustLoginMiddleware::class]);
+$router->post("/user/profile", "UserController@postUpdateProfile", [OnlyMemberMiddleware::class]);
 
-$router->get("/user/password", "UserController@updatePassword", [MustLoginMiddleware::class]);
-$router->post("/user/password", "UserController@postupdatePassword", [MustLoginMiddleware::class]);
+$router->get("/user/password", "UserController@updatePassword", [OnlyMemberMiddleware::class]);
+$router->post("/user/password", "UserController@postupdatePassword", [OnlyMemberMiddleware::class]);
 
 $router->get($request->getPath(), function() {
     response()->setStatus(404);

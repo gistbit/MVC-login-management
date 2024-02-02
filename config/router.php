6a5a847f6aa8a879c484150db64ed6846eb3c220
gistbit <muh.akram1422@gmail.com>
2024-02-02 18:48:06 +0800

    menambahkan middleware

--- config/router.php
+++ config/router.php
@@ -16,7 +16,7 @@ $router->post("/user/login", "UserController@postLogin", [OnlyGuestMiddleware::c
 
 $router->get("/user/logout", "UserController@logout", [OnlyMemberMiddleware::class]);
 
-$router->get("/user/profile", "UserController@updateProfile", [MustLoginAdmin::class]);
+$router->get("/user/profile", "UserController@updateProfile", [OnlyMemberMiddleware::class, MustLoginAdmin::class]);
 $router->post("/user/profile", "UserController@postUpdateProfile", [OnlyMemberMiddleware::class]);
 
 $router->get("/user/password", "UserController@updatePassword", [OnlyMemberMiddleware::class]);

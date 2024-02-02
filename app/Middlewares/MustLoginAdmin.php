b50684ac775a364a39758293a48eb0926a1cea36
gistbit <muh.akram1422@gmail.com>
2024-02-02 19:07:52 +0800

    refactoring

--- app/Middlewares/MustLoginAdmin.php
+++ app/Middlewares/MustLoginAdmin.php
@@ -3,8 +3,6 @@
 namespace App\Middlewares;
 
 use App\Core\Http\Request;
-use App\Core\Http\Response;
-use App\Core\MVC\View;
 
 use function App\helper\response;
 
@@ -16,14 +14,7 @@ class MustLoginAdmin implements Middleware
         if($session !== null && $session->role == 1){
             return true;
         }
-        response()->setStatus(403);
-        response()->setContent(View::renderViewOnly(404, [
-            'title' => Response::STATUS_TEXTS[403],
-            'status' => [
-                'code' => 403,
-                'text' => Response::STATUS_TEXTS[403]
-            ]
-        ]));
+        response()->setNotFound();
         return false;
     }
 }

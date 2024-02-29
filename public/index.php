d06364392218616cdeee2820ff61c0220b84bdfd
gistbit <muh.akram1422@gmail.com>
2024-02-29 22:56:43 +0800

    refactoring code

--- public/index.php
+++ public/index.php
@@ -1,5 +1,6 @@
 <?php
-// load autoload file
+
+// Load the autoload file
 require_once dirname(__DIR__) . '/vendor/autoload.php';
 
 use MA\PHPMVC\Core\App;
@@ -7,12 +8,16 @@ use MA\PHPMVC\Core\Http\Request;
 use MA\PHPMVC\Core\Http\Response;
 use MA\PHPMVC\Core\Router\Router;
 
+// Create instances
 $request = new Request();
 $response = new Response();
 $router = new Router();
 
+// Load routes configuration
 require_once CONFIG . '/router.php';
 
+// Initialize the application
 $app = new App($request, $response);
 
+// Run the application with the provided router
 $app->run($router);

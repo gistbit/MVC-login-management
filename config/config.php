4e9e90d8fd24cfc169775db5bb5bc0876585b44a
gistbit <muh.akram1422@gmail.com>
2024-02-29 16:45:56 +0800

    change key session

--- config/config.php
+++ config/config.php
@@ -18,7 +18,7 @@ return [
 
     'session' => [
         'name' => 'PHP-MVC',
-        'key' => "WjBGSFNHdHBhbXBLUkVwWlYxZFZNREk0T1RFd1NrWkxSa05PU2t0QlNVbFBTVWhKUVU5UFNUa3pPRk5CUm10elpHRmtZWE5yYW1wMmRYWTRNamt3TlRneU1qbHVjMnRxWm1Gb1lXeGhMSHB0ZUcxclkyWnBNVFk0TWprek1HNW1hR1k",
+        'key' => "kRd9SO75b0MffA6ThNjW0lYfZpUJzwbiwN9moDf0wQvyLWmBdrnYbCZ4IekHQVNenFD8gt4sKreL7ZV0bUR32I5j8NexHITW5PTTHkm6GqjezcitHuQRfD1I3v1r7xgG2MDZWvLEmMF8140u1jtiyODLPIxU00mYMFfoyP0pRcQKeKx8NOKzPNmBYYNCB97xEHTZ6d1cJWipsg5a7k3ZkWWpXivBaWXHcdZ9E34al4EHY2DVgWwYHDtQ3jUSjuG8",
         'exp' => time() + ( 60 * 60 * 3) // 3 JAM
     ]
 ];

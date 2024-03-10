c311329de3423daa70ac59ef81f1657e239ba3b1
gistbit <muh.akram1422@gmail.com>
2024-03-11 01:03:35 +0800

    refactoring code

--- config/config.php
+++ config/config.php
@@ -22,6 +22,6 @@ return [
     'session' => [
         'name' => 'PHP-MVC',
         'key' => "kRd9SO75b0MffA6ThNjW0lYfZpUJzwbiwN9moDf0wQvyLWmBdrnYbCZ4IekHQVNenFD8gt4sKreL7ZV0bUR32I5j8NexHITW5PTTHkm6GqjezcitHuQRfD1I3v1r7xgG2MDZWvLEmMF8140u1jtiyODLPIxU00mYMFfoyP0pRcQKeKx8NOKzPNmBYYNCB97xEHTZ6d1cJWipsg5a7k3ZkWWpXivBaWXHcdZ9E34al4EHY2DVgWwYHDtQ3jUSjuG8",
-        'exp' => time() + ( 60 * 60 * 3) // 3 JAM
+        'exp' => time() + (60 * 60 * 3) // 3 JAM
     ]
 ];

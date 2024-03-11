b42b69cbaf340d1fe3baacea0539f4146926a272
gistbit <muh.akram1422@gmail.com>
2024-03-11 13:13:10 +0800

    refactoring code

--- config/constants.php
+++ config/constants.php
@@ -1,5 +1,5 @@
 <?php
-// path root
+// Path
 define('ROOT', str_replace('\\', '/', rtrim(dirname(__DIR__), '/')));
 define('APP', ROOT . '/app');
 define('CONTROLLERS', APP . '/Controllers');
@@ -10,7 +10,6 @@ define('CONFIG', ROOT . '/config');
 define('DOC_ROOT', str_replace('\\', '/', rtrim($_SERVER['DOCUMENT_ROOT'], '/')));
 define('VENDOR', ROOT . '/vendor');
 
-// base URL
+// Base URL
 define('BASE_URL', "http://www.localhost:8080");
-// URL sumber menggunakan base URL
 define('UPLOAD_URL', BASE_URL . '/upload');

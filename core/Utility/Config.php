35ade792b4d2523273886505408f90ce97aacb83
Akram <105730626+gistbit@users.noreply.github.com>
2024-04-02 18:09:05 +0800

    refactoring code

--- core/Utility/Config.php
+++ core/Utility/Config.php
@@ -4,6 +4,8 @@ namespace MA\PHPMVC\Utility;
 
 class Config
 {
+    protected static $config;
+
     public static function isDevelopmentMode(): bool
     {
         return self::get('mode.development', false);
@@ -14,9 +16,18 @@ class Config
         return self::get('database', []);
     }
 
+    protected static function loadConfig()
+    {
+        if (!isset(self::$config)) {
+            self::$config = require(CONFIG . '/config.php');
+        }
+    }
+
     public static function get($key, $default = null)
     {
-        $config = require(CONFIG . '/config.php');
+        self::loadConfig();
+
+        $config = self::$config;
         $keys = explode('.', $key);
 
         foreach ($keys as $part) {

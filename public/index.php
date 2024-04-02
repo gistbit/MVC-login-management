b7d7405272b9c520673d85405b77c4175bb84b15
Akram <105730626+gistbit@users.noreply.github.com>
2024-04-03 06:04:46 +0800

    refactoring code

--- public/index.php
+++ public/index.php
@@ -1,11 +1,11 @@
 <?php
 
 // Include the autoloader to load necessary classes
-require_once dirname(__DIR__) . '/vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
 // Load the router by including the bootstrap.php file
 // The bootstrap.php file is expected to return an instance of the router
-$router = require_once dirname(__DIR__) . '/bootstrap.php';
+$router = require_once __DIR__ . '/../bootstrap.php';
 
 // Run the router
 $response = $router->run();

cab228526a3b86f3b4528f35ca7a9c983c128dbe
Akram <105730626+gistbit@users.noreply.github.com>
2024-04-16 17:02:11 +0800

    Update migration.php

--- migration.php
+++ migration.php
@@ -5,10 +5,10 @@ require_once __DIR__ . '/vendor/autoload.php';
 use MA\PHPMVC\Database\Database;
 
 try {
-    $dbh = Database::getConnection();
+    $db = Database::getConnection();
     
     // Buat tabel users
-    $dbh->exec("CREATE TABLE IF NOT EXISTS users (
+    $db->exec("CREATE TABLE IF NOT EXISTS users (
         id VARCHAR(255) PRIMARY KEY,
         name VARCHAR(255) NOT NULL,
         password VARCHAR(255) NOT NULL,
@@ -18,7 +18,7 @@ try {
     echo "Tabel users berhasil dibuat atau sudah ada.\n";
     
     // Buat tabel sessions
-    $dbh->exec("CREATE TABLE IF NOT EXISTS sessions (
+    $db->exec("CREATE TABLE IF NOT EXISTS sessions (
         id VARCHAR(255) PRIMARY KEY,
         user_id VARCHAR(255) NOT NULL,
         FOREIGN KEY (user_id) REFERENCES users(id)

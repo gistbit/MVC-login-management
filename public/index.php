928c520d5264fa0e393edd776261914ce1847a8a
Akram <105730626+gistbit@users.noreply.github.com>
2024-05-08 20:38:10 +0800

    Update index.php

--- public/index.php
+++ public/index.php
@@ -5,7 +5,7 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 // Load the router by including the bootstrap.php file
 // The bootstrap.php file is expected to return an instance of the router
-$router = require_once __DIR__ . '/../bootstrap.php';
+$router = require __DIR__ . '/../bootstrap.php';
 
 // Run the router
 $response = $router->run();

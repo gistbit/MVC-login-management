bf0a2da760d51b377dae0400d08b539db8c7ac77
Akram <105730626+gistbit@users.noreply.github.com>
2024-05-28 06:47:36 +0800

    Update index.php

--- public/index.php
+++ public/index.php
@@ -3,9 +3,18 @@
 // Include the autoloader to load necessary classes
 require_once __DIR__ . '/../vendor/autoload.php';
 
-// Load the router by including the bootstrap.php file
-// The bootstrap.php file is expected to return an instance of the router
-$router = require __DIR__ . '/../bootstrap.php';
+use MA\PHPMVC\Http\Request;
+use MA\PHPMVC\Http\Response;
+use MA\PHPMVC\Router\Router;
+
+// Path constants
+define('DIR_ROOT', str_replace('\\', '/', dirname(__DIR__)));
+define('CONFIG', DIR_ROOT . '/config');
+define('VIEWS', DIR_ROOT . '/app/views');
+
+require CONFIG . '/routes.php';
+
+$router = new Router(new Request(), new Response());
 
 // Run the router
 $response = $router->run();
